02335dcad761199599358ace01219cb5927d7c33
SMatakane <matakane558@gmail.com>
2022-08-31 06:00:19 -0700

    high-level

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,6 +5,6 @@ def complex_delete(a_dictionary, value):
     while value in a_dictionary.values():
         for k, v in a_dictionary.items():
             if v == value:
-                del a_dictionary
+                del a_dictionary[k]
                 break
     return (a_dictionary)
