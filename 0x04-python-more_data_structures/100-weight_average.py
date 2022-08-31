8c19f9d00c3357f57ef03471eb749c40d80dfffa
SMatakane <matakane558@gmail.com>
2022-08-31 05:53:48 -0700

    high-level

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 
 
 def weight_average(my_list=[]):
-    if not isinstance(my_list, list) or len (my_list) == 0:
+    if not isinstance(my_list, list) or len(my_list) == 0:
         return (0)
 
     avg = 0
